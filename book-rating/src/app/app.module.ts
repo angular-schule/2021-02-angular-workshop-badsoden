b00dddc1302be608dd9b119fedde4d0433a2f972
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-02-24 14:12:01 +0100

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 // import { DashboardComponent } from './dashboard/dashboard.component';
 // import { BookComponent } from './book/book.component';
@@ -19,7 +23,10 @@ import { BooksModule } from './books/books.module';
     BrowserModule,
     AppRoutingModule,
     // BooksModule, // because of lazy loading
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
