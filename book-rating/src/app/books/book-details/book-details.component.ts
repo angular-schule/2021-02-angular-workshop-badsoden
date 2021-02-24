55da9c8fafa80c8c3a46373f58fcd14c0647be6d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-02-24 10:38:04 +0100

    async pipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { concatMap, map, mergeMap, switchMap } from 'rxjs/operators';
+import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.scss']
+  styleUrls: ['./book-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookDetailsComponent {
 
-  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+  book$ = this.route.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn')),
+    switchMap(isbn => this.bs.getSingleBook(isbn))
+  );
 
-    this.route.paramMap.pipe(
-      map(paramMap => paramMap.get('isbn')),
-      switchMap(isbn => this.bs.getSingleBook(isbn))
-    ).subscribe(book => console.log(book));
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
   }
 
 
