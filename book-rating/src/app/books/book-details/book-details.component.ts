55d2869942213ab51159a49abbff702ca9c71f68
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-02-24 09:30:28 +0100

    rxjs ANTIPATTERN: nested subscribe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  constructor(private route: ActivatedRoute) {
-    this.route.paramMap
-      .subscribe(paramMap => console.log(paramMap.get('isbn')));
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+
+    this.route.paramMap.pipe(
+      map(paramMap => paramMap.get('isbn')),
+      map(isbn => this.bs.getSingleBook(isbn))
+    ).subscribe(book$ => book$.subscribe(console.log));
   }
 
-  ngOnInit(): void {
-  }
 
 }
